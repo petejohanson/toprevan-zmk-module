1ef77d2ac16a281898a8918a48a5f5e1278be2aa
Peter Johanson <peter@peterjohanson.com>
2025-05-29 23:00:27 -0600

    fix: Fixes for full functionality of TopreVan.

--- boards/arm/calvin_toprevan/board.cmake
+++ boards/arm/calvin_toprevan/board.cmake
@@ -4,6 +4,7 @@
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
 board_runner_args(jlink "--device=STM32F401CC" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
